0567ff2b77ca3892ad840c10d00082ed7ac396d5
Guilherme <guiminha.w@outlook.com>
2020-06-07 19:16:34 -0300

    quinto dia: finalizando a aplicação, trabalhando com o sqlite3 e dando os retoques finais.

--- public/scripts/create-point.js
+++ public/scripts/create-point.js
@@ -1,91 +1,95 @@
-
 function populateUFs() {
-  const ufSelect = document.querySelector("select[name=uf]");
+   const ufSelect = document.querySelector("select[name=uf]");
 
-  fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
-  .then( res => res.json() )
-  .then( states => {
+   fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+      .then(res => res.json())
+      .then(states => {
 
-    for ( const state of states ) {
-      ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`;
-    }      
-  })
+         for (const state of states) {
+            ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`;
+         }
+      })
 }
 
 populateUFs();
 
 function getCities(event) {
-  const citySelect = document.querySelector("select[name=city]");
-  const stateInput = document.querySelector("input[name=state]");
+   const citySelect = document.querySelector("select[name=city]");
+   const stateInput = document.querySelector("input[name=state]");
 
-  const ufValue = event.target.value;
+   const ufValue = event.target.value;
 
-  const indexOfSelectedState = event.target.selectedIndex
-  stateInput.value = event.target.options[indexOfSelectedState].text;
+   const indexOfSelectedState = event.target.selectedIndex
+   stateInput.value = event.target.options[indexOfSelectedState].text;
 
-  const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`;
+   const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`;
 
-  fetch(url)
-  .then( res => res.json() )
-  .then( cities => {
+   citySelect.innerHTML = "<option value>Selecione a Cidade</option>";
+   citySelect.disable = true;
 
-    for ( const city of cities ) {
-      citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`;
-    }      
+   fetch(url)
+      .then(res => res.json())
+      .then(cities => {
 
-    citySelect.disabled = false;
+         for (const city of cities) {
+            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`;
+         }
 
-  })
+         citySelect.disabled = false;
+
+      })
 }
 
 document
-  .querySelector("select[name=uf]")
-  .addEventListener("change", getCities)
+   .querySelector("select[name=uf]")
+   .addEventListener("change", getCities)
 
 //Itens coleta
-
 //Pegar todos os 'li'
-
 const itemsToCollect = document.querySelectorAll(".items-grid li");
 
 for (const item of itemsToCollect) {
-  item.addEventListener("click", handleSelectedItem);
+   item.addEventListener("click", handleSelectedItem);
 }
 
 const collectedItems = document.querySelector("input[name=items]");
 
 let selectedItems = [];
 
-function handleSelectedItem(event){
-  
-  const itemLi = event.target;
+function handleSelectedItem(event) {
 
-  itemLi.classList.toggle("selected"); 
+   const itemLi = event.target;
 
-  const itemId = itemLi.dataset.id;
+   itemLi.classList.toggle("selected");
 
-  // verificar se existem itens selecionados, se sim 
-  // pegar os itens selecionados
+   const itemId = itemLi.dataset.id;
 
-  const alreadySelected = selectedItems.findIndex( item => {
-    const itemFound = item == itemId; 
-    return itemFound;
-  })
+   console.log('ITEM ID: ', itemId);
 
-  // se já estiver selecionado, tirar da seleção
-  if (alreadySelected >= 0){
-    const filteredItems = selectedItems.filter( item => {
-      const itemsIsDifferent = item != itemId;
-      return itemsIsDifferent;
-    })
-  
-    selectedItems = filteredItems;
-  
-  } else {
-    // se não tiver selecionado, adicionar a seleção
-    selectedItems.push(itemId);
-  }
-  
-  // atualizar o campo escondido com os itens selecionados
-  collectedItems.value = selectedItems;
+   // verificar se existem itens selecionados, se sim 
+   // pegar os itens selecionados
+
+   const alreadySelected = selectedItems.findIndex(item => {
+      const itemFound = item == itemId;
+      return itemFound;
+   })
+
+   // se já estiver selecionado, tirar da seleção
+   if (alreadySelected >= 0) {
+      const filteredItems = selectedItems.filter(item => {
+         const itemsIsDifferent = item != itemId;
+         return itemsIsDifferent;
+      })
+
+      selectedItems = filteredItems;
+
+   } else {
+      // se não tiver selecionado, adicionar a seleção
+      selectedItems.push(itemId);
+   }
+
+   console.log('selectedItems: ', selectedItems);
+
+   // atualizar o campo escondido com os itens selecionados
+   collectedItems.value = selectedItems;
 }
---
+++ src/database/db.js
@@ -0,0 +1,83 @@
+// importar sqlite3 
+const sqlite3 = require("sqlite3").verbose();
+
+// criar o objeto para operar o banco de dados
+const db = new sqlite3.Database("./src/database/database.db");
+
+module.exports = db;
+
+//utilizar o objeto para as operações no banco
+db.serialize(() => {
+
+//    // criar uma tabela com sqlite3
+//    db.run(`
+//       CREATE TABLE IF NOT EXISTS places (
+//          id INTEGER PRIMARY KEY AUTOINCREMENT,
+//          image TEXT,
+//          name TEXT,
+//          address TEXT,
+//          address2 TEXT,
+//          state TEXT,
+//          city TEXT,
+//          items TEXT 
+//       );
+//    `);
+
+//    // inserir dados na tabela
+//    const query = `
+//       INSERT INTO places (
+//          image, 
+//          name,
+//          address,
+//          address2,
+//          state,
+//          city,
+//          items
+//       ) VALUES (?,?,?,?,?,?,?);   
+//    `;
+
+//    const values = [
+//       "https://images.unsplash.com/photo-1528323273322-d81458248d40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1101&q=80",
+//       "Papersider",
+//       "Guilherme Gemballa, Jardim América",
+//       "Número 360",
+//       "Santa Catarina",
+//       "Rio do Sul",
+//       "Papéis e Papelão"
+//    ];
+
+//    function afterInsertData(err) {
+//       if (err) {
+//          return console.log(err);
+//       }
+
+//       console.log("Cadastrado com sucesso");
+//       console.log(this);
+//    }
+
+//     db.run(query, values, afterInsertData);
+
+//    // consultar dados
+
+  
+// //    // deletar dados 
+
+//    // db.run(`DELETE FROM places`, function(err){
+//    //    if (err) {
+//    //       return console.log(err);
+//    //    }
+
+//    //    console.log("Registro deletado com sucesso!");
+
+//    // });
+
+   db.all(` SELECT * FROM places`, function(err, rows) {      
+      if (err) {
+         return console.log(err);
+      }
+
+      console.log("Aqui estão os seus registros");
+      console.log(rows);
+   })
+
+})
